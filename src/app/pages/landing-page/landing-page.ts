752b40fa8c012511aab61325764fd64303d5eeed
JohnLloydProg <johnlloydunida0@gmail.com>
2026-01-22 03:56:26 +0800

    Finished the our story section. A bit more polishing is needed.

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, signal, ViewChild } from '@angular/core';
 import { StackCarousel } from '../../components/stack-carousel/stack-carousel';
-import { ScrollTrapDirective } from "../../directives/scroll-trap-directive";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [StackCarousel, ScrollTrapDirective],
+  imports: [StackCarousel, NgClass],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
 export class LandingPage {
+  @ViewChild('track') track! : ElementRef;
+  contentTransform = signal('translateY(0px)');
+  private scrollTimeout:any;
+  public progress = 0;
+
+  @HostListener('wheel', ['$event'])
+  onWheel(event:WheelEvent) {
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      const trackEl = this.track.nativeElement;
+      const rect = trackEl.getBoundingClientRect();
+
+      const distFromTop = -rect.top;
+
+      const totalDistance = trackEl.offsetHeight - window.innerHeight;
+
+      let preProgress = distFromTop / totalDistance;
+
+      this.progress = Math.max(0, Math.min(preProgress, 1)) * 100;
+      
+    }, 10);
+    
+
+  }
 
 }
